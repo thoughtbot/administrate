bbe4ff45f8796bf4e73aa00e4a64329efa839e18
Grayson Wright <wright.grayson@gmail.com>
2015-04-22 23:26:00 -0700

    Create Orders and Line Items in db/seeds
    Needed to reduce the number of users from 100 to 20,
    because we're getting a performance hit on the Orders index page.
    
    I'm guessing the cause is an N+1 query for the
    `Order.belongs_to :customer` relationship,
    but I haven't investigated yet.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+LineItem.destroy_all
+Order.destroy_all
 Customer.destroy_all
 Product.destroy_all
 
-100.times do
+20.times do
   name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
   Customer.create(
     name: name,
@@ -21,4 +23,27 @@ product_attributes = YAML.load_file("./db/seeds/products.yml")
 
 product_attributes.each do |attributes|
   Product.create attributes.merge(price: 20 + rand(50))
+end
+
+Customer.all.each do |customer|
+  (1..3).to_a.sample.times do
+    order = Order.create(
+      customer: customer,
+      address_line_one: Faker::Address.street_address,
+      address_line_two: Faker::Address.secondary_address,
+      address_city: Faker::Address.city,
+      address_state: Faker::Address.state_abbr,
+      address_zip: Faker::Address.zip,
+    )
+
+    item_count = (1..3).to_a.sample
+    Product.all.sample(item_count).each do |product|
+      LineItem.create(
+        order: order,
+        product: product,
+        unit_price: product.price,
+        quantity: (1..3).to_a.sample,
+      )
+    end
+  end
 end
