a85b9fb68d90d4cd12147db05f27553d01aa3e01
Steven & Grayson <grayson+steven@thoughtbot.com>
2015-05-15 11:03:03 -0700

    Revisit layout and table styles
    - Center layout, with "floating" sidebar. Inspired by heroku's dashboard
    - Make entire table rows clickable on index pages
    - Style form page
    - Style header (h1 and links)
    - Style select menus
    - Choose new blues for accents and backgrounds
    - Define a grey scale from 0 (light) to 7 (dark)
      http://color.hailpixel.com/#263238,344346,4E6A68,648687,90ACAD,AABDC0,E4EDF6,ECF3F9,

---
+++ app/assets/javascripts/components/table.js
@@ -0,0 +1,18 @@
+$(function() {
+  sap = {ui:{keycodes:{SPACE:32, ENTER:13 }}};
+
+  var visitDataUrl = function(event) {
+
+    if (event.type=="click" ||
+        event.keyCode == sap.ui.keycodes.SPACE ||
+        event.keyCode == sap.ui.keycodes.ENTER) {
+
+      if(!event.target.href) {
+        window.location = event.target.closest("tr").dataset["url"]
+      }
+    }
+  }
+
+  $("table").on("click", ".table-row", visitDataUrl);
+  $("table").on("keydown", ".table-row", visitDataUrl);
+});
