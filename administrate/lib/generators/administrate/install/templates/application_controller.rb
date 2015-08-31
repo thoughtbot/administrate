1d6a714359bd260332f05acac4c67669496c3d61
Grayson Wright <wright.grayson@gmail.com>
2015-08-31 14:00:59 -0700

    Improve flash performance and formatting
    Why?
    
    - Flashes were being set using the `flash[:alert] = ` syntax, which
      saves the flash for the next request the user makes (used during
      redirects, etc).
    - When users visited the index page and then clicked through to another
      page, they would still see the index page's flash from the previous
      request.
    
    This commit switches to the `flash.now[:alert] = ` syntax, which only
    sets the flash for the current request.
    
    Also, switch flashes to use [BEM syntax].
    
    [BEM syntax]: http://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax/

--- administrate/lib/generators/administrate/install/templates/application_controller.rb
+++ administrate/lib/generators/administrate/install/templates/application_controller.rb
@@ -14,7 +14,7 @@ class Admin::ApplicationController < Administrate::ApplicationController
   def index
     super
 
-    flash[:alert] =
+    flash.now[:alert] =
       "For performance, Administrate limits the index page to show 20 items.
       Customize this action to update/remove the limit,
       or implement the pagination library of your choice."
