0f708cab2868968cd5556777ae8683bb355f41ad
Grayson Wright <wright.grayson@gmail.com>
2015-09-11 17:11:22 -0700

    Preserve whitespace for strings on show page
    Problem: Long description fields were being all mashed onto a single
    line on the 'show' page, making it difficult to read.
    
    https://trello.com/c/xueiirQV
    
    Solution: Add a CSS class to mark the type of each attribute on the show
    page, then add a rule to preserve whitespace.

--- spec/lib/fields/string_spec.rb
+++ spec/lib/fields/string_spec.rb
@@ -1,7 +1,10 @@
 require "spec_helper"
 require "administrate/fields/string"
+require "support/field_matchers"
 
 describe Administrate::Field::String do
+  include FieldMatchers
+
   describe "#to_partial_path" do
     it "returns a partial based on the page being rendered" do
       page = :show
@@ -12,4 +15,6 @@ describe Administrate::Field::String do
       expect(path).to eq("/fields/string/#{page}")
     end
   end
+
+  it { should_permit_param(:foo, for_attribute: :foo) }
 end
