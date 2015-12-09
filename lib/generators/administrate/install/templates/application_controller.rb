194de11760c4d2826e6e6b10dc03696b6daca278
Sean Doyle <sean.p.doyle24@gmail.com>
2015-12-09 11:43:02 -0500

    Generated nested Admin::ApplicationController
    The current generator for `Admin::ApplicationController` uses the
    compact style, which is a styleguide violation for our Rubopcop (and
    transitively Hound) configuration.

--- lib/generators/administrate/install/templates/application_controller.rb
+++ lib/generators/administrate/install/templates/application_controller.rb
@@ -4,16 +4,18 @@
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
-class Admin::ApplicationController < Administrate::ApplicationController
-  before_filter :authenticate_admin
+module Admin
+  class ApplicationController < Administrate::ApplicationController
+    before_filter :authenticate_admin
 
-  def authenticate_admin
-    # TODO Add authentication logic here.
+    def authenticate_admin
+      # TODO Add authentication logic here.
+    end
+
+    # Override this value to specify the number of elements to display at a time
+    # on index pages. Defaults to 20.
+    # def records_per_page
+    #   params[:per_page] || 20
+    # end
   end
-
-  # Override this value to specify the number of elements to display at a time
-  # on index pages. Defaults to 20.
-  # def records_per_page
-  #   params[:per_page] || 20
-  # end
 end
