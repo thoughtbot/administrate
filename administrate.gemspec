ecc6782a8b127e76ce414e0aa48962ec4711699b
Mario Alberto Chávez <mario.chavez@gmail.com>
2015-12-24 18:52:41 -0600

    Removes inline_svg dependency.
    `inline_svg` gem is currently broken to work with Sprockets 3 and the
    Rails application environment set to production.
    So this has broken `administrate` as well.
    Normally, a fix should be implemented in broken gem but after looking at
    why `administrate` requires `inline_svg` gem I realize that it is easy
    to replace it with a helper method.
    
    `ApplicationHelper#svg_tag` method was added to substitute the work of
    `inline_svg`. This method creates a svg tag that will look like:
    
        <svg xmlns="http://www.w3.org/2000/svg"
        xmlns:xlink="http://www.w3.org/1999/xlink"><use
        xlink:href="http://localhost:3000/assets/administrate/search-360c782d91a21edd53f5754c3fd8f8984ec5d9b36389f6df1fe618a4e50405b6.svg#search" width="16" height="16"></use></svg>
    
    This will allow to resize the svg image and to use css to change
    properties like color.
    
    But there are a couple of differences with `inline_svg`.
    First the svg file should have an id property,
    for my previous example it is `search`.
    
    The second difference is that `use` tag needs to have
    `height` and `width` properties set as attributes of the tag, css
    properties will not affect the svg size.
    
    `ApplicationHelper#svg_tag` used to generate html above looks like:
    
      <%= svg_tag "administrate/search.svg",
        svg_id: "search", width: "16", height: "16" %>
    
    This solution will play nice with Sprockets and with modern browsers.

--- administrate.gemspec
+++ administrate.gemspec
@@ -16,7 +16,6 @@ Gem::Specification.new do |s|
 
   s.add_dependency "autoprefixer-rails", "~> 6.0"
   s.add_dependency "datetime_picker_rails", "~> 0.0.6"
-  s.add_dependency "inline_svg", "~> 0.6"
   s.add_dependency "jquery-rails", "~> 4.0"
   s.add_dependency "kaminari", "~> 0.16"
   s.add_dependency "momentjs-rails", "~> 2.8"
--- spec/helpers/administrate/application_helper_spec.rb
+++ spec/helpers/administrate/application_helper_spec.rb
@@ -41,4 +41,35 @@ RSpec.describe Administrate::ApplicationHelper do
       end
     end
   end
+
+  describe "#svg_tag" do
+    it "returns use tag with svg file path in xlink:href attribute" do
+      use_tag = build_svg_tag.xpath("//svg//use['xlink:href']")
+
+      expect(use_tag.size).to eq(1)
+      expect(use_tag[0].attributes["xlink:href"].value).to eq("/logo.svg#logo")
+    end
+
+    context "with size attributes" do
+      it "returns use tag with height" do
+        svg_tag = build_svg_tag(height: 15)
+        use_tag = svg_tag.xpath("//svg//use['height']")
+        attribute = use_tag[0].attributes["height"]
+
+        expect(attribute.value).to eq("15")
+      end
+
+      it "returns use tag with width" do
+        svg_tag = build_svg_tag(width: 20)
+        use_tag = svg_tag.xpath("//svg//use['width']")
+        attribute = use_tag[0].attributes["width"]
+
+        expect(attribute.value).to eq("20")
+      end
+    end
+
+    def build_svg_tag(options = {})
+      Nokogiri::HTML svg_tag("logo.svg", "logo", options)
+    end
+  end
 end
