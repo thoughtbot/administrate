1846bd71ab0087f8d13499f61406bb7773bb5b6c
Ben A. Morgan <ben@benmorgan.io>
2015-12-31 13:48:49 -0500

    use before_action instead of before_filter

--- lib/generators/administrate/install/templates/application_controller.rb
+++ lib/generators/administrate/install/templates/application_controller.rb
@@ -1,12 +1,12 @@
 # All Administrate controllers inherit from this `Admin::ApplicationController`,
 # making it the ideal place to put authentication logic or other
-# before_filters.
+# before_actions.
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_filter :authenticate_admin
+    before_action :authenticate_admin
 
     def authenticate_admin
       # TODO Add authentication logic here.
