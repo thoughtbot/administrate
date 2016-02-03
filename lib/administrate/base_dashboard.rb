c1563793347b93012705f45c61a1f78999467d45
Jan Berdajs <mrbrdo@gmail.com>
2016-02-03 15:39:48 +0100

    allow for a Field to permit multiple attributes

--- lib/administrate/base_dashboard.rb
+++ lib/administrate/base_dashboard.rb
@@ -35,7 +35,7 @@ module Administrate
     end
 
     def permitted_attributes
-      form_attributes.map do |attr|
+      form_attributes.flat_map do |attr|
         attribute_types[attr].permitted_attribute(attr)
       end.uniq
     end
