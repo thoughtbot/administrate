5a8987a2940037849a1b135813ce77bdb450264f
Paul Bennett-Freeman <clicknmix@gmail.com>
2016-03-02 17:24:10 +0000

    Add `format` option to Administrate::Field::DateTime
     * Allow a DateTime field to be configured as
     `.with_options( format: <format> )`.
    
     Formatting options can be found at
     http://ruby-doc.org/stdlib-2.2.3/libdoc/date/rdoc/DateTime.html#method-i-strftime

--- lib/administrate/field/date_time.rb
+++ lib/administrate/field/date_time.rb
@@ -3,6 +3,17 @@ require_relative "base"
 module Administrate
   module Field
     class DateTime < Base
+      def to_s
+        if data.nil?
+          ""
+        else
+         data.strftime(format_date) 
+        end
+      end
+
+      def format_date
+        options[:format] || "%FT%T%:z"
+      end
     end
   end
 end
--- spec/lib/fields/date_time_spec.rb
+++ spec/lib/fields/date_time_spec.rb
@@ -1,6 +1,39 @@
-require "spec_helper"
 require "administrate/field/date_time"
 require "support/field_matchers"
 
 describe Administrate::Field::DateTime do
+  include FieldMatchers
+
+  let :datetime_to_test do
+    DateTime.new(1979,10,15,13,37) 
+  end 
+
+  describe "#to_s" do
+    it "formats the sting using defaults" do
+      page = :show
+      field = Administrate::Field::DateTime.new(:datetime, datetime_to_test, page)
+
+      expect(field.to_s).to eq "1979-10-15T13:37:00+00:00"
+    end
+
+    context "with `format` option" do
+      it "displays with the given format" do
+
+        date = datetime_with_options(datetime_to_test, format: "%Y")
+        expect(date.to_s).to eq("1979")
+      end
+    end
+
+    context "when data is nil" do
+      it "returns an empty string" do
+        field = Administrate::Field::DateTime.new(:datetime, nil, :page)
+
+        expect(field.to_s).to eq("")
+      end
+    end
+  end
+
+  def datetime_with_options(datetime, options)
+      Administrate::Field::DateTime.new(:datetime, datetime, :page, options)
+  end
 end
