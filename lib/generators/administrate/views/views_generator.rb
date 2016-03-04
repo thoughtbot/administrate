df6ce0f871666d5f56834add16d902d5ec471358
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2016-03-04 12:24:08 +0700

    [#484] Fix views generator should pass args to sub-generator

--- lib/generators/administrate/views/views_generator.rb
+++ lib/generators/administrate/views/views_generator.rb
@@ -4,10 +4,10 @@ module Administrate
   module Generators
     class ViewsGenerator < Administrate::ViewGenerator
       def copy_templates
-        call_generator("administrate:views:index", resource_path)
-        call_generator("administrate:views:show", resource_path)
-        call_generator("administrate:views:new", resource_path)
-        call_generator("administrate:views:edit", resource_path)
+        call_generator("administrate:views:index", *args)
+        call_generator("administrate:views:show", *args)
+        call_generator("administrate:views:new", *args)
+        call_generator("administrate:views:edit", *args)
       end
     end
   end
