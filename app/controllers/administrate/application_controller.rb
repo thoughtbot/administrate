be738a54b866191bf49664d8c6116587fb971759
Tute Costa <tutecosta@gmail.com>
2016-04-01 10:44:06 -0400

    Use Administrate::ApplicationController.protect_from_forgery
    `Administrate::ApplicationController` didn't call
    `protect_from_forgery`, which means that non-GET methods don’t validate
    CSRF tokens. Thus, it is possible for an attacker to hijack user
    session's and act on administrate actions on their behalf.
    
    Thanks to Jason Yeo (@jsyeo) of SRC:CLR for finding and reporting this
    vulnerability.
    
    CVE-2016-3098

--- app/controllers/administrate/application_controller.rb
+++ app/controllers/administrate/application_controller.rb
@@ -1,5 +1,7 @@
 module Administrate
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
     def index
       search_term = params[:search].to_s.strip
       resources = Administrate::Search.new(resource_resolver, search_term).run
