3910b94bdcd1304c47add730c4e0037acf0b7435
James Cox <james@imaj.es>
2016-05-10 15:45:54 -0400

    Switch before filter > action in template.

--- lib/generators/administrate/install/templates/application_controller.rb
+++ lib/generators/administrate/install/templates/application_controller.rb
@@ -6,7 +6,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_filter :authenticate_admin
+    before_action :authenticate_admin
 
     def authenticate_admin
       # TODO Add authentication logic here.
