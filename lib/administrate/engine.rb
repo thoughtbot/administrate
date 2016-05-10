e4cc0921fba9bf713ac810161725e9b13e9102e2
Grayson Wright <wright.grayson@gmail.com>
2016-05-10 07:53:46 -0700

    Add engine class methods for registering assets
    Problem:
    
    When developing a plugin for Administrate,
    there is not a good way to define custom javascripts or stylesheets
    to be included on admin pages.
    
    Existing solutions either use `content_for` at the field level,
    or require the end user to override Administrate's javascript partial.
    
    Solution:
    
    Add methods to `Administrate::Engine` to keep track of the assets
    that need to be loaded on each admin page.
    
    Plugin developers can now use these helpers in their engine definition:
    
    ```ruby
    Administrate::Engine.add_javascript "my_plugin/script"
    Administrate::Engine.add_stylesheet "my_plugin/styles"
    ```

--- lib/administrate/engine.rb
+++ lib/administrate/engine.rb
@@ -20,6 +20,28 @@ module Administrate
   class Engine < ::Rails::Engine
     isolate_namespace Administrate
 
+    @@javascripts = []
+    @@stylesheets = []
+
     Engine.config.assets.precompile << /\.(?:svg)\z/
+
+    def self.add_javascript(script)
+      @@javascripts << script
+    end
+
+    def self.add_stylesheet(stylesheet)
+      @@stylesheets << stylesheet
+    end
+
+    def self.stylesheets
+      @@stylesheets
+    end
+
+    def self.javascripts
+      @@javascripts
+    end
+
+    add_javascript "administrate/application"
+    add_stylesheet "administrate/application"
   end
 end
