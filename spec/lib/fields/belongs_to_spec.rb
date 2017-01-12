5ebdb1a47b53cb020db2a5ae75a6b797392c1690
Nick Pezza <npezza93@gmail.com>
2017-01-12 15:18:25 -0500

    Allow primary_key option on relationships instead of hard coding id (#729)
    Currently BelongsTo and HasMany default to using #id of the
    related records. This will allow you to pass the option of primary_key
    and use that fields' value in the form.

--- spec/lib/fields/belongs_to_spec.rb
+++ spec/lib/fields/belongs_to_spec.rb
@@ -37,4 +37,33 @@ describe Administrate::Field::BelongsTo do
       end
     end
   end
+
+  describe "primary_key option" do
+    it "determines what primary key is used on the relationship for the form" do
+      begin
+        Foo = Class.new
+        FooDashboard = Class.new
+        uuid = SecureRandom.uuid
+        allow(Foo).to receive(:all).and_return([Foo])
+        allow(Foo).to receive(:uuid).and_return(uuid)
+        allow(Foo).to receive(:id).and_return(1)
+        allow_any_instance_of(FooDashboard).to(
+          receive(:display_resource).and_return(uuid)
+        )
+
+        association =
+          Administrate::Field::BelongsTo.with_options(
+            primary_key: "uuid", class_name: "Foo"
+          )
+        field = association.new(:customers, [], :show)
+        field.associated_resource_options
+
+        expect(Foo).to have_received(:all)
+        expect(Foo).to have_received(:uuid)
+        expect(Foo).not_to have_received(:id)
+      ensure
+        remove_constants :Foo, :FooDashboard
+      end
+    end
+  end
 end
