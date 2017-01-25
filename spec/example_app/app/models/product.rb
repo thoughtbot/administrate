16e7490489b31e24da2c4837b1da9820e45ca7ca
Nick Charlton <nick@nickcharlton.net>
2017-01-25 11:29:26 +0000

    Products also need to destroy dependent models.
    This is related to #739 and #738.

--- spec/example_app/app/models/product.rb
+++ spec/example_app/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
   validates :description, presence: true
   validates :image_url, presence: true
   validates :name, presence: true
