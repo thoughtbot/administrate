80757e310c2ab28d2a72f4c9095d56ad3701d3cb
Peter Schilling <peter@peterschilling.org>
2017-03-10 16:18:50 -0800

    Use a time format locale for HH:MM:SS

--- spec/lib/fields/time_spec.rb
+++ spec/lib/fields/time_spec.rb
@@ -3,17 +3,4 @@ require "administrate/field/time"
 require "support/field_matchers"
 
 describe Administrate::Field::Time do
-  include FieldMatchers
-
-  describe "#default" do
-    it "returns a formatted string of the time" do
-      page = :show
-      example_time = DateTime.new(2000, 1, 1, 15, 45, 33)
-      field = Administrate::Field::Time.new(:owner, example_time, page)
-
-      default = field.default
-
-      expect(default).to eq("15:45:33")
-    end
-  end
 end
