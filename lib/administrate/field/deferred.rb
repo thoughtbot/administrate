ae98f993f77c23fecf927d772da1b97d5ffa3c71
Rich Daley <rich@fishpercolator.co.uk>
2017-03-24 10:25:09 +0000

    Follow up on review by @BenMorganIO and write a spec for this

--- lib/administrate/field/deferred.rb
+++ lib/administrate/field/deferred.rb
@@ -11,7 +11,8 @@ module Administrate
       attr_reader :deferred_class, :options
 
       def new(*args)
-        new_options = if args.last.is_a? Hash then args.pop else {} end
+        # If the args already end with a "keyword hash", merge it with options
+        new_options = args.last.respond_to?(:merge) ? args.pop : {}
         deferred_class.new(*args, options.merge(new_options))
       end
 
