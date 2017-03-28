1445da46d190642b15cc26e4e846957cbaae4f8f
Josh Morrow <josh@jcmorrow.com>
2017-03-28 09:04:29 -0700

    Fix spec

--- spec/dashboards/payment_dashboard_spec.rb
+++ spec/dashboards/payment_dashboard_spec.rb
@@ -5,6 +5,11 @@ describe PaymentDashboard do
     context "when there is foreign key that is not `id`" do
       it "interpolates in the correct column name" do
         dashboard = PaymentDashboard.new
+        allow(dashboard).to receive(:attribute_types).and_return(
+          { id: Administrate::Field::Number,
+            order: Administrate::Field::BelongsTo.with_options(primary_key: :code)
+          }
+        )
 
         expect(dashboard.permitted_attributes).to include(:order_code)
       end
