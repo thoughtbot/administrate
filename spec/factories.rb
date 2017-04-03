0190528c26cd9459915bec840924d2413b8bdef0
Klas Eskilson <klas.eskilson@gmail.com>
2017-04-03 15:53:42 +0200

    Add spec for `sort_by` and `direction` in HasMany field

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,16 @@ FactoryGirl.define do
   factory :customer do
     sequence(:name) { |n| "Customer #{n}" }
     email { name.downcase.gsub(" ", "_") + "@example.com" }
+
+    transient do
+      order_count 3
+    end
+
+    trait :with_orders do
+      after(:create) do |customer, evaluator|
+        create_list(:order, evaluator.order_count, customer: customer)
+      end
+    end
   end
 
   factory :line_item do
@@ -14,7 +24,7 @@ FactoryGirl.define do
   factory :order do
     customer
     address_line_one "85 2nd St"
-    address_line_two "#700"
+    sequence(:address_line_two) { |n| "#700 (#{n})" }
     address_city "San Francisco"
     address_state "CA"
     address_zip "94110"
