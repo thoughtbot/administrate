188e3bb9546ae2b67bd30ceeeee9c6dca9d3859f
Ben A. Morgan <ben@benmorgan.io>
2017-05-01 15:28:37 -0400

    convert some views to render_page

--- lib/administrate/field/has_many.rb
+++ lib/administrate/field/has_many.rb
@@ -45,6 +45,10 @@ module Administrate
         data.count(:all) > limit
       end
 
+      def render_page
+        render_index if page == :index
+      end
+
       private
 
       def candidate_resources
@@ -70,6 +74,10 @@ module Administrate
 
       def direction
         options[:direction]
+      end
+
+      def render_index
+        attribute.to_s.humanize.downcase.pluralize data.size
       end
     end
   end
