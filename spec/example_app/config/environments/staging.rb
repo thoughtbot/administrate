e3f107af4ce81c6cdc3dcc33f0dae5aef8c5429a
Roman Sokhan <pustomytnyk@gmail.com>
2017-05-02 14:58:46 +0300

    Remove dependency from ActionMailer

--- spec/example_app/config/environments/staging.rb
+++ spec/example_app/config/environments/staging.rb
@@ -1,11 +1,1 @@
 require_relative "production"
-
-Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
-)
-
-Rails.application.configure do
-  # ...
-
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
-end
