26915671086525b9a3517b78204697cd9e1f67b0
Dmitry Solomadin <dmitry.solomadin@shopify.com>
2017-05-08 15:08:00 -0400

    Rebase off master

--- spec/example_app/db/schema.rb
+++ spec/example_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507115814) do
+ActiveRecord::Schema.define(version: 20170508183744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
