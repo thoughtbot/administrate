50b6cfdad519e00f44f37427c660b89a388aed24
Oli Peate <oli@thoughtbot.com>
2017-05-12 17:12:16 +0100

    Test documentation navigation

--- spec/features/documentation_spec.rb
+++ spec/features/documentation_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
-describe "documentation" do
+describe "documentation navigation" do
+  it "links to each documentation page" do
+    visit root_path
+    links = internal_documentation_links
+
+    expect(links).to_not be_empty
+
+    links.each do |link|
+      visit link
+      expect(page).to have_http_status(:ok), "Unable to find #{link}"
+    end
+  end
+
   it "links to the Github repo" do
     visit root_path
 
@@ -12,5 +24,11 @@ describe "documentation" do
 
   def github_link
     first(".sidebar-links").find("a", text: "GitHub")
+  end
+
+  def internal_documentation_links
+    all(".sidebar a").
+      map { |anchor| anchor[:href] }.
+      select { |href| URI.parse(href).relative? }
   end
 end
