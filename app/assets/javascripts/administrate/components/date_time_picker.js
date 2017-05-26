5e348ffc11eb6b3589d4b4b041f81e1d17bf71df
Enrique Barragan <ebarragan1997@gmail.com>
2017-05-25 23:26:54 -0500

    Added Time Field

--- app/assets/javascripts/administrate/components/date_time_picker.js
+++ app/assets/javascripts/administrate/components/date_time_picker.js
@@ -1,4 +1,8 @@
 $(function () {
+  $(".timepicker").datetimepicker({
+  debug: false,
+  format: "HH:mm:ss",
+  });
   $(".datetimepicker").datetimepicker({
     debug: false,
     format: "YYYY-MM-DD HH:mm:ss",
--- lib/administrate/base_dashboard.rb
+++ lib/administrate/base_dashboard.rb
@@ -9,6 +9,7 @@ require "administrate/field/polymorphic"
 require "administrate/field/select"
 require "administrate/field/string"
 require "administrate/field/text"
+require "administrate/field/time"
 
 module Administrate
   class BaseDashboard
