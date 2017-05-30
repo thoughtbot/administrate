29121fe5b1077a6e7c5fd95996b31bd75917bfec
iarie <litvinov@applicot.ru>
2017-05-30 18:49:39 +0300

    Rails release for migration

--- spec/example_app/db/migrate/20170510122301_create_countries.rb
+++ spec/example_app/db/migrate/20170510122301_create_countries.rb
@@ -1,4 +1,4 @@
-class CreateCountries < ActiveRecord::Migration
+class CreateCountries < ActiveRecord::Migration[4.2]
   def change
     create_table :countries do |t|
       t.string :code, null: false, index: { unique: true }
