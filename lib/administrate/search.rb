d70a769a09b86b7db7d848dfc2bdb3993d555014
Jason Rogers <jason@getupperhand.com>
2017-06-19 20:15:11 -0400

    at first, the Hound-ing was fun, now it's just plain annoying at 80-character line lengths; it's 2017 -- can't we bump this up to 100 at least

--- lib/administrate/search.rb
+++ lib/administrate/search.rb
@@ -4,13 +4,20 @@ require "active_support/core_ext/object/blank"
 module Administrate
   class Search
     class SearchStrategy
-      def initialize(search_attributes, term, resource_class: nil, table_name: nil)
+      def initialize(
+        search_attributes,
+        term,
+        resource_class: nil,
+        table_name: nil
+      )
         if table_name
           @table_name = table_name
         elsif resource_class
-          @table_name = ActiveRecord::Base.connection.quote_table_name(resource_class.table_name)
+          @table_name = ActiveRecord::Base.connection.quote_table_name(
+            resource_class.table_name,
+          )
         else
-          raise ArgumentError, 'resource_class or table_name is required'
+          raise ArgumentError, "resource_class or table_name is required"
         end
         @search_attributes, @term = search_attributes, term
       end
@@ -33,7 +40,10 @@ module Administrate
 
       def query
         @search_attributes.map do |name, attr|
-          attr.searchable.with_context(@term).query(@table_name, attr_name(name))
+          attr.searchable.with_context(@term).query(
+            @table_name,
+            attr_name(name),
+          )
         end.join(" OR ")
       end
 
@@ -76,9 +86,14 @@ module Administrate
       elsif term.nil?
         search_strategy_cls = NilSearchStrategy
       else
-        raise ArgumentError, "cannot determine search strategy for a #{term.class}"
+        raise ArgumentError,
+          "cannot determine search strategy for a #{term.class}"
       end
-      @search_strategy = search_strategy_cls.new(search_attributes, @term, resource_class: resource_class)
+      @search_strategy = search_strategy_cls.new(
+        search_attributes,
+        @term,
+        resource_class: resource_class,
+      )
     end
 
     def run
