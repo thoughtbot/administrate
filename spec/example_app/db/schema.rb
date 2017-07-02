bcb6d1a324fca664c5e9f001aab6c2ed0acb8691
Andre Anastacio <andreluisanastacio@gmail.com>
2017-07-02 00:44:01 -0300

    Remove NameError rescue in page base

--- spec/example_app/db/schema.rb
+++ spec/example_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507115814) do
+ActiveRecord::Schema.define(version: 20170702033920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20170507115814) do
 
   create_table "payments", id: :serial, force: :cascade do |t|
     t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
