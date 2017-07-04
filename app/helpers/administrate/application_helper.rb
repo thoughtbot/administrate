d1175dc55c2c292e0ce0b29527bb5aba1632f088
Jon Kinney <jonkinney@gmail.com>
2017-07-03 23:42:46 -0500

    Fix 'found unpermitted parameter: :orders' dynamically

--- app/helpers/administrate/application_helper.rb
+++ app/helpers/administrate/application_helper.rb
@@ -28,7 +28,21 @@ module Administrate
     end
 
     def sanitized_order_params
-      params.permit(:search, :id, :order, :page, :per_page, :direction, :orders)
+      resources =
+        Administrate::Namespace.new(:admin).
+          resources.
+          map(&:resource).
+          map(&:to_sym)
+
+      params.permit(
+        *resources,
+        :search,
+        :id,
+        :order,
+        :page,
+        :per_page,
+        :direction,
+      )
     end
 
     def clear_search_params
