1a7e591b0c868dc545dc8b3bd357f58c25b2b24e
Anton Ilin <anton@ilin.dn.ua>
2017-07-19 10:57:36 +0300

    Move l18n call from partial to the field implementation

--- lib/administrate/field/date.rb
+++ lib/administrate/field/date.rb
@@ -3,6 +3,15 @@ require_relative "base"
 module Administrate
   module Field
     class Date < Base
+      def date
+        I18n.localize(data.to_date, format: format)
+      end
+
+      private
+
+      def format
+        options.fetch(:format, :default)
+      end
     end
   end
 end
--- lib/administrate/field/time.rb
+++ lib/administrate/field/time.rb
@@ -3,6 +3,15 @@ require_relative "base"
 module Administrate
   module Field
     class Time < Base
+      def time
+        I18n.localize(data, format: format)
+      end
+
+      private
+
+      def format
+        options.fetch(:format, :time_without_date)
+      end
     end
   end
 end
