9695e74880982290faef101cccc8f9bf4a35bd0a
Ryan Hertz <rhertz@fbsdata.com>
2017-10-01 12:36:38 -0400

    added 'all' option to views field generator

--- spec/generators/views/field_generator_spec.rb
+++ spec/generators/views/field_generator_spec.rb
@@ -1,9 +1,11 @@
-require "spec_helper"
+require "rails_helper"
 require "generators/administrate/views/field_generator"
 require "support/generator_spec_helpers"
 
 describe Administrate::Generators::Views::FieldGenerator, :generator do
+
   context "for an existing field type" do
+
     describe "administrate:views:field field_name" do
       it "copies the `_show` partial into the app/views/fields directory" do
         expected_contents = contents_for_field_template(:string, :show)
@@ -32,6 +34,46 @@ describe Administrate::Generators::Views::FieldGenerator, :generator do
         expect(contents).to eq(expected_contents)
       end
     end
+
+    describe "administrate:views:field all" do
+
+      let(:field_types) { Dir.entries("app/views/fields").reject{ |name| name[0] == "."} }
+
+      it "copies the `_show` partial for each field type" do
+        run_generator ["all"]
+
+        field_types.each do |field_type|
+          expected_contents = contents_for_field_template(field_type, :show)
+          contents = File.read(file("app/views/fields/#{field_type}/_show.html.erb"))
+
+          expect(contents).to eq(expected_contents)
+        end
+      end
+
+      it "copies the `_form` partial for each field type" do
+        run_generator ["all"]
+
+        field_types.each do |field_type|
+          expected_contents = contents_for_field_template(field_type, :form)
+          contents = File.read(file("app/views/fields/#{field_type}/_form.html.erb"))
+
+          expect(contents).to eq(expected_contents)
+        end
+      end
+
+      it "copies the `_index` partial for each field type" do
+        run_generator ["all"]
+
+        field_types.each do |field_type|
+          expected_contents = contents_for_field_template(field_type, :index)
+          contents = File.read(file("app/views/fields/#{field_type}/_index.html.erb"))
+
+          expect(contents).to eq(expected_contents)
+        end
+      end
+
+    end
+
   end
 
   def contents_for_field_template(field_name, partial_name)
