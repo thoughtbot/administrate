29166a33cdd4f09df3fe8d7aa330503e2d90343d
Joel Oliveira <jayroh@users.noreply.github.com>
2017-11-23 10:47:46 -0500

    Option for Date(Time) objects be **in a timezone** (#1009)
    Often times a layperson using a dashboard can be misled when they look
    at a record's creation/update time and not realize it's rooted in the
    UTC timezone. This commit adds the option to display `Field::DateTime`
    fields as being in a provided timezone.
    
    Also - update the documentation to contain the two options for that
    field: `:format` and `:timezone`.
    
    Note: By default the timezone in that field is explicitly set as UTC.

--- lib/administrate/field/date_time.rb
+++ lib/administrate/field/date_time.rb
@@ -4,17 +4,28 @@ module Administrate
   module Field
     class DateTime < Base
       def date
-        I18n.localize(data.to_date, format: format)
+        I18n.localize(
+          data.in_time_zone(timezone).to_date,
+          format: format,
+        )
       end
 
       def datetime
-        I18n.localize(data, format: format, default: data)
+        I18n.localize(
+          data.in_time_zone(timezone),
+          format: format,
+          default: data,
+        )
       end
 
       private
 
       def format
         options.fetch(:format, :default)
+      end
+
+      def timezone
+        options.fetch(:timezone, "UTC")
       end
     end
   end
