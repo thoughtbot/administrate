71257942c56e84b620224e60df0e0628847b55ea
Ryan Hertz <rhertz@fbsdata.com>
2018-01-20 11:38:04 -0500

    fixed false pass in has_many _form spec

--- spec/administrate/views/fields/has_many/_form_spec.rb
+++ spec/administrate/views/fields/has_many/_form_spec.rb
@@ -1,27 +1,28 @@
 require "rails_helper"
+require "administrate/field/has_many"
 
 describe "fields/has_many/_form", type: :view do
   describe "the field label" do
     it "displays the association name" do
-      has_many = double(
-        attribute_key: :associated_object_ids,
-        attribute: :associated_objects,
-      )
+      has_many = Administrate::Field::HasMany.new(:associated_objects, [],
+        :some_page)
+
+      allow(has_many).to receive(:associated_resource_options).and_return([])
 
       render(
         partial: "fields/has_many/form.html.erb",
         locals: { f: fake_form_builder, field: has_many },
       )
 
-      expect(rendered).to include("Associated Objects")
+      expect(rendered).to include("Associated objects")
     end
   end
 
   def fake_form_builder
-    double("Form Builder").as_null_object.tap do |form_builder|
-      allow(form_builder).to receive(:label) do |*args|
-        args.second.to_s.titleize
-      end
-    end
+    object_name = "some object"
+    object = double("object", associated_object_ids: [])
+    template = ActionController::Base.new.view_context
+    options = {}
+    ActionView::Helpers::FormBuilder.new(object_name, object, template, options)
   end
 end
