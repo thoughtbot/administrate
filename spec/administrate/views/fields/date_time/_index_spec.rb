496f452fbe737b7c3798732a602043ee71b4bf40
mark100net <mark_github@pokervane.com>
2018-04-01 15:06:28 -0700

    fix formatting issues

--- spec/administrate/views/fields/date_time/_index_spec.rb
+++ spec/administrate/views/fields/date_time/_index_spec.rb
@@ -4,14 +4,16 @@ describe "fields/date_time/_index", type: :view do
   it "renders date_time" do
     product = create(:product)
     date_time = instance_double(
-         "Administrate::Field::DateTime.with_options(format: #{Time::DATE_FORMATS[:default]})",
-         data: product,
-         datetime: product.created_at
+      "Administrate::Field::DateTime.with_options(
+        format: #{Time::DATE_FORMATS[:default]}
+      )",
+      data: product,
+      datetime: product.created_at,
     )
 
     render(
-        partial: "fields/date_time/index.html.erb",
-        locals: { field: date_time, namespace: "admin" },
+      partial: "fields/date_time/index.html.erb",
+      locals: { field: date_time, namespace: "admin" },
     )
   end
 end
