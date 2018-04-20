4eaef9e8589b505eb548715d053f9289f0e40059
Dmitry Shvetsov <sorrows@mail.ru>
2018-04-20 23:27:20 +1000

    Fix code style issues in Administrate::ApplicationController (#1138)
    * Refactor a long line ApplicationController#index
    
    This change fixes a violation of the line length rule
    https://rubocop.readthedocs.io/en/latest/cops_metrics/#metricslinelength
    
    * Fix code style issues in ApplicationController
    
    Fix memoized ivar name issues ApplicationController
    https://rubocop.readthedocs.io/en/latest/cops_naming/#namingmemoizedinstancevariablename
    
    Fix unused method arguments
    https://rubocop.readthedocs.io/en/latest/cops_lint/#lintunusedmethodargument
    
    Fix trailing comma in arguments
    https://rubocop.readthedocs.io/en/latest/cops_style/#styletrailingcommainarguments

--- app/controllers/administrate/application_controller.rb
+++ app/controllers/administrate/application_controller.rb
@@ -7,7 +7,7 @@ module Administrate
       resources = Administrate::Search.new(scoped_resource,
                                            dashboard_class,
                                            search_term).run
-      resources = resources.includes(*resource_includes) if resource_includes.any?
+      resources = apply_resource_includes(resources)
       resources = order.apply(resources)
       resources = resources.page(params[:page]).per(records_per_page)
       page = Administrate::Page::Collection.new(dashboard, order: order)
@@ -16,7 +16,7 @@ module Administrate
         resources: resources,
         search_term: search_term,
         page: page,
-        show_search_bar: show_search_bar?
+        show_search_bar: show_search_bar?,
       }
     end
 
@@ -101,15 +101,15 @@ module Administrate
     end
 
     def order
-      @_order ||= Administrate::Order.new(params[:order], params[:direction])
+      @order ||= Administrate::Order.new(params[:order], params[:direction])
     end
 
     def dashboard
-      @_dashboard ||= dashboard_class.new
+      @dashboard ||= dashboard_class.new
     end
 
     def requested_resource
-      @_requested_resource ||= find_resource(params[:id]).tap do |resource|
+      @requested_resource ||= find_resource(params[:id]).tap do |resource|
         authorize_resource(resource)
       end
     end
@@ -122,8 +122,10 @@ module Administrate
       resource_class.default_scoped
     end
 
-    def resource_includes
-      dashboard.association_includes
+    def apply_resource_includes(relation)
+      resource_includes = dashboard.association_includes
+      return relation if resource_includes.empty?
+      relation.includes(*resource_includes)
     end
 
     def resource_params
@@ -150,7 +152,7 @@ module Administrate
     helper_method :resource_name
 
     def resource_resolver
-      @_resource_resolver ||=
+      @resource_resolver ||=
         Administrate::ResourceResolver.new(controller_path)
     end
 
@@ -163,11 +165,11 @@ module Administrate
 
     def show_search_bar?
       dashboard.attribute_types_for(
-        dashboard.collection_attributes
+        dashboard.collection_attributes,
       ).any? { |_name, attribute| attribute.searchable? }
     end
 
-    def show_action?(action, resource)
+    def show_action?(_action, _resource)
       true
     end
     helper_method :show_action?
