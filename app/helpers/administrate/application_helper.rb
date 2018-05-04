d1031ea17b514f4d59c16931ce50257617d83a7d
Eric Collins <eric@tabfugni.cc>
2018-05-04 09:48:11 -0400

    Allow parameters that are more permissive
    When using the Clearance gem's backdoor to access views in
    administrate, tests prematurely break because of the `as` parameter
    used to do quick sign in. While it's probably fair that we don't want
    persist that parameter through the GET request, it shouldn't matter
    what parameters we pass in a GET request.
    
    * Use `permit!` to allow anything through on a GET request

--- app/helpers/administrate/application_helper.rb
+++ app/helpers/administrate/application_helper.rb
@@ -32,18 +32,12 @@ module Administrate
       ActiveModel::Naming.route_key(class_from_resource(resource_name))
     end
 
-    def sanitized_order_params(page, current_field_name)
-      collection_names = page.association_includes + [current_field_name]
-      association_params = collection_names.map do |assoc_name|
-        { assoc_name => %i[order direction page per_page] }
-      end
-      params.permit(:search, :id, :page, :per_page, association_params)
+    def sanitized_order_params
+      params.permit!
     end
 
     def clear_search_params
-      params.except(:search, :page).permit(
-        :per_page, resource_name => %i[order direction]
-      )
+      params.except(:search, :page).permit!
     end
   end
 end
