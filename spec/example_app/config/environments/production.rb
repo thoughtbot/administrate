ecf988685ad1bf99e581023e704da365ded4a391
Nick Charlton <nick@nickcharlton.net>
2018-09-17 11:32:49 +0100

    Update rack-timeout from 0.4.2 to 0.5.1.

--- spec/example_app/config/environments/production.rb
+++ spec/example_app/config/environments/production.rb
@@ -77,5 +77,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
