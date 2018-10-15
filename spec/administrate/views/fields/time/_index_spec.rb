f7c6b93d27911946773c14c3a7e69d810c32b63c
rnitta <attinyes@gmail.com>
2018-10-15 15:33:52 +0900

    Add spec

---
+++ spec/administrate/views/fields/time/_index_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "fields/time/_index", type: :view do
+  context "time value is nil" do
+    it "display nothing" do
+      time = double(data: nil)
+
+      render(
+        partial: "fields/time/index.html.erb",
+        locals: { field: time },
+      )
+
+      expect(rendered.strip).to eq("")
+    end
+  end
+  context "time value is set" do
+    it "renders time" do
+      example_time = "12:34:00"
+      customer = create(:customer, example_time: example_time)
+      time = instance_double(
+        "Administrate::Field::Time",
+        data: customer.example_time,
+      )
+      render(
+        partial: "fields/time/index.html.erb",
+        locals: { field: time, namespace: "admin" },
+      )
+
+      expect(rendered.strip).to eq("12:34PM")
+    end
+  end
+end
