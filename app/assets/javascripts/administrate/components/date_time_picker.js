35b7d90f83ed27d28e821ec36c57bd2064c83673
Mauricio Zaffari <mauriciozaffari@gmail.com>
2018-12-18 12:44:34 -0200

    Adds default date to datetimepicker

--- app/assets/javascripts/administrate/components/date_time_picker.js
+++ app/assets/javascripts/administrate/components/date_time_picker.js
@@ -3,18 +3,21 @@ $(function () {
     $(this).datetimepicker({
       debug: false,
       format: $(this).data('format') || 'HH:mm:ss',
+      defaultDate: Date.parse($(this).val()),
     });
   });
   $('[data-type="date"]').each(function() {
     $(this).datetimepicker({
       debug: false,
       format: $(this).data('format') || 'YYYY-MM-DD',
+      defaultDate: Date.parse($(this).val()),
     });
   });
   $('[data-type="datetime"]').each(function() {
     $(this).datetimepicker({
       debug: false,
       format: $(this).data('format') || 'YYYY-MM-DD HH:mm:ss',
+      defaultDate: Date.parse($(this).val()),
     });
   });
 });
