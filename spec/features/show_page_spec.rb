a07ea02bc71552e00bcef4d2e471d06df1001e8f
Pablo Brasero <pablo@pablobm.com>
2019-01-04 18:30:01 +0100

    Fix multiple association pagination (#1268)
    On a dashboard with multiple associations, pagination was only valid for one
    and would otherwise cause an unpermitted parameter exception.

--- spec/features/show_page_spec.rb
+++ spec/features/show_page_spec.rb
@@ -27,6 +27,26 @@ RSpec.describe "customer show page" do
       ids_in_table = (ids_in_page1 + ids_in_page2).uniq
       expect(ids_in_table).to match_array(order_ids)
     end
+
+    describe(
+      "when these are not a collection field" +
+      "and there's another paging association",
+    ) do
+      it "doesn't break" do
+        orig_collection_attributes = CustomerDashboard::COLLECTION_ATTRIBUTES
+        allow_any_instance_of(CustomerDashboard).to(
+          receive(:collection_attributes).
+          and_return(orig_collection_attributes - [:orders]),
+        )
+
+        customer = create(:customer)
+        create_list(:order, 4, customer: customer)
+        create_list(:log_entry, 1, logeable: customer)
+
+        visit admin_customer_path(customer)
+        click_on("Next ›")
+      end
+    end
   end
 
   it "displays the customer's title attribute as the header" do
