0331f1a2652a7bfa650b501ee2362490f3428e89
Chris Oliver <excid3@gmail.com>
2019-02-28 17:53:08 -0600

    Eager load when using zeitwork to find all models

--- lib/generators/administrate/routes/routes_generator.rb
+++ lib/generators/administrate/routes/routes_generator.rb
@@ -9,6 +9,10 @@ module Administrate
       class_option :namespace, type: :string, default: "admin"
 
       def insert_dashboard_routes
+        if Rails.configuration.try(:autoloader) == :zeitwerk
+          Rails.autoloaders.main.eager_load
+        end
+
         if should_route_dashboard?
           route(dashboard_routes)
         end
