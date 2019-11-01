56fefb5dd3d2b30a918221516c020b6e05e5d3bc
Pablo Brasero <pablo@pablobm.com>
2019-11-01 16:57:03 +0000

    Otherwise, it produces file:// URLs on Linux

--- app/assets/javascripts/administrate/components/table.js
+++ app/assets/javascripts/administrate/components/table.js
@@ -13,7 +13,7 @@ $(function() {
       var dataUrl = $(event.target).closest("tr").data("url");
       var selection = window.getSelection().toString();
       if (selection.length === 0 && dataUrl) {
-        window.location = dataUrl;
+        window.location.pathname = dataUrl;
       }
     }
   };
