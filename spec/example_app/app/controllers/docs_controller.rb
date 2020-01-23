8710d091c0f7c0baeed62b45b128968c83104425
Pablo Brasero <pablo@pablobm.com>
2020-01-23 15:44:17 +0000

    Provide <title> tags for doc pages

--- spec/example_app/app/controllers/docs_controller.rb
+++ spec/example_app/app/controllers/docs_controller.rb
@@ -18,7 +18,11 @@ class DocsController < ApplicationController
     path = full_page_path(name)
 
     if File.exist?(path)
-      render layout: "docs", html: render_markdown(path).html_safe
+      contents = parse_markdown(path)
+      @page_title = contents.title
+      # rubocop:disable Rails/OutputSafety
+      render layout: "docs", html: contents.body.html_safe
+      # rubocop:enable Rails/OutputSafety
     else
       render file: "#{Rails.root}/public/404.html",
              layout: false,
@@ -30,11 +34,38 @@ class DocsController < ApplicationController
     Rails.root + "../../#{page}.md"
   end
 
-  def render_markdown(path)
+  def parse_markdown(path)
     text = File.read(path)
-    renderer = Redcarpet::Render::HTML
-    markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
+    MarkdownParser.new(text)
+  end
 
-    markdown.render(text)
+  class MarkdownParser
+    def initialize(source_text)
+      @source_text = source_text
+    end
+
+    def body
+      @body ||=
+        begin
+          renderer = Redcarpet::Render::HTML
+          markdown = Redcarpet::Markdown.new(renderer, REDCARPET_CONFIG)
+
+          markdown.render(source_text)
+        end
+    end
+
+    def title
+      @title ||=
+        begin
+          h1_match = @source_text.scan(/^# (.*)$/).first
+          raise "Please provide an H1 heading for the page" if h1_match.empty?
+
+          h1_match.first
+        end
+    end
+
+    private
+
+    attr_reader :source_text
   end
 end
