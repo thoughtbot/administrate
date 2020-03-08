3719ce5c255fda8da048653d2f449e847157f687
Pablo Brasero <pablo@pablobm.com>
2020-03-08 19:20:07 +0000

    Tables won't always be named after models

--- spec/example_app/app/models/customer.rb
+++ spec/example_app/app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
-  belongs_to :country, foreign_key: :country_code, primary_key: :code
+  belongs_to(
+    :territory,
+    class_name: "Country",
+    foreign_key: :country_code,
+    primary_key: :code,
+  )
   has_many :log_entries, as: :logeable
 
   validates :name, presence: true
