65e9b3c57a19691abd222d58a9fd7031080512bc
Emily Wright <56832601+EWright212@users.noreply.github.com>
2020-07-24 11:45:10 +0100

    Refactor remove single quotes
    Co-authored-by: Nick Charlton <nick@nickcharlton.net>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path("../../spec/example_app/config/environment", __FILE__)
 
 require "rspec/rails"
 require "shoulda/matchers"
-require 'selenium/webdriver'
+require "selenium/webdriver"
 
 Dir[Rails.root.join("../../spec/support/**/*.rb")].each { |file| require file }
 
