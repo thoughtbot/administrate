2296176187a7db99e8c55edd72457069571e16d8
Nick Charlton <nick@nickcharlton.net>
2020-10-06 20:09:31 +0100

    Switch to using suspender's Capybara configuration
    With more recent Chromedriver versions, we need to specify the headless
    window size which this does. Plus more familiarity with this
    configuration.

---
+++ spec/support/webdrivers.rb
@@ -0,0 +1,30 @@
+require "selenium/webdriver"
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.headless!
+  options.add_argument "--window-size=1680,1050"
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options,
+  )
+end
+
+Capybara.javascript_driver = :headless_chrome
+Capybara.server = :webrick
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by Capybara.javascript_driver
+  end
+end
