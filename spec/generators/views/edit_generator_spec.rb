0c6382a321dac0ca797ae9a9f37743546d9b5f8c
Rachael Ewins <rachaelewins@hotmail.com>
2020-11-25 15:51:02 +0000

    Added namespace can be specified from CLI for views generators
    Co-authored-by: Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>

--- spec/generators/views/edit_generator_spec.rb
+++ spec/generators/views/edit_generator_spec.rb
@@ -41,4 +41,24 @@ describe Administrate::Generators::Views::EditGenerator, :generator do
       expect(contents).to eq(expected_contents)
     end
   end
+
+  describe "administrate:views:edit resource --namespace <namespace>" do
+    it "copies the edit template into the `<namespace>/resource` namespace" do
+      expected_contents = contents_for_application_template("edit")
+
+      run_generator ["LineItem", "--namespace", "test"]
+      contents = File.read(file("app/views/test/line_items/edit.html.erb"))
+
+      expect(contents).to eq(expected_contents)
+    end
+
+    it "copies the form partial into the `admin/resource` namespace" do
+      expected_contents = contents_for_application_template("_form")
+
+      run_generator ["users", "--namespace", "test2"]
+      contents = File.read(file("app/views/test2/users/_form.html.erb"))
+
+      expect(contents).to eq(expected_contents)
+    end
+  end
 end
