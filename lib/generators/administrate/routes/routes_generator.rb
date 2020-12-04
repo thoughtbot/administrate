2d7aba5234b11c1dc59b3c7fcd62382ae87822d3
Rachael Ewins <rachaelewins@hotmail.com>
2020-12-04 21:08:04 +0000

    Fix presentation in config/routes.rb

--- lib/generators/administrate/routes/routes_generator.rb
+++ lib/generators/administrate/routes/routes_generator.rb
@@ -47,6 +47,36 @@ module Administrate
         end
       end
 
+      def dashboard_resources_hash()
+        output = [{}, []]
+        dashboard_resources.each do |resource_path| 
+          split_path = resource_path.split('/')
+          output = [
+            namespaces(split_path, output[0]),
+            resources(split_path, output[1])
+          ]
+        end
+        output
+      end
+      
+      def namespaces(split_path, current_state)
+        if split_path.length > 1
+          current_state[split_path[0]] ||= [{}, []]
+          current_state[split_path[0]] = [
+            namespaces(split_path[1..], current_state[split_path[0]][0]),
+            resources(split_path[1..], current_state[split_path[0]][1]),
+          ]
+        end
+        current_state
+      end
+      
+      def resources(split_path, current_state)
+        if split_path.length == 1
+          current_state.push(split_path[0])
+        end
+        current_state
+      end
+
       def valid_dashboard_models
         database_models - (invalid_dashboard_models + excluded_models).uniq
       end
@@ -73,25 +103,41 @@ module Administrate
         ActiveRecord::Base.descendants.reject { |d| d.name == d.to_s }
       end
 
-      def generate_resource_routes(resource)
-        if resource.include?("/")
-          parts = resource.split("/")
-          generate_nested_resource_routes(parts)
-        else
-          "resources :#{resource}"
+      def generate_resource_routes(namespace)
+        output_string = ""
+        indentation = "  "
+        namespace[1].each do |resource|
+          output_string += "  resource :#{resource}\n"
         end
+        output_string += "\n"
+        namespace[0].each do |namespace, namespace_resource|
+          output_string += "#{indentation}namespace :#{namespace} do\n"
+          namespace_resource[1].each do |resource|
+            output_string += "#{indentation}  resource :#{resource}\n"
+          end
+          namespace_resource[0].each do |namespace, namespace_resource|
+            output_string += generate_namespace_routes(namespace)
+          end
+          output_string += "#{indentation}end"
+        end
+        output_string += "\n"
+        output_string
       end
 
-      def generate_nested_resource_routes(items, index = 0)
-        if index + OFFSET == items.size
-          "resources :#{items[index]}"
-        else
-          resource_indent = " " * ((index + TWO_INDENT) * DEFAULT_INDENT)
-          resource = generate_nested_resource_routes(items, index + OFFSET)
-          indent_resource = "#{resource_indent}#{resource}"
-          indent_end = " " * ((index + ONE_INDENT) * DEFAULT_INDENT) + "end"
-          "namespace :#{items[index]} do\n#{indent_resource}\n#{indent_end}"
+      def generate_namespace_routes(hash, indentation_level = 2) 
+        output_string = ""
+        indentation = "  " * indentation_level
+        hash.each do |namespace, namespace_resource|
+          output_string += "#{indentation}namespace :#{namespace} do\n"
+          namespace_resource[1].map do |resource|
+            output_string += "#{indentation}  resource :#{resource}\n"
+          end   
+          namespace_resource[0].each do |namespace, namespace_resource|
+            output_string += generate_namespace(namespace, indentation_level + 1)
+          end
+          output_string += "#{indentation}end"
         end
+        output_string
       end
 
       def dashboard_routes
