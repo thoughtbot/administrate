5846e550449a2192192e163db30ddca26dd1f441
Nic Hippenmeyer <nic@rinsed.co>
2020-12-17 13:35:46 -0600

    Add view spec

---
+++ spec/administrate/views/fields/select/_edit_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+require "administrate/field/select"
+
+describe "fields/select/_form", type: :view do
+  it "displays the selected option" do
+    customer = build(:customer)
+    select = instance_double(
+      "Administrate::Field::Select",
+      attribute: :email_subscriber,
+      data: false,
+      selectable_options: [true, false, nil],
+    )
+
+    render(
+      partial: "fields/select/form.html.erb",
+      locals: { field: select, f: form_builder(customer) },
+    )
+
+    expect(rendered).to have_css(
+      %{select[name="customer[email_subscriber]"]
+        option[value="false"][selected="selected"]},
+    )
+  end
+
+  def form_builder(object)
+    ActionView::Helpers::FormBuilder.new(
+      object.model_name.singular,
+      object,
+      build_template,
+      {},
+    )
+  end
+
+  def build_template
+    Object.new.tap do |template|
+      template.extend ActionView::Helpers::FormHelper
+      template.extend ActionView::Helpers::FormOptionsHelper
+    end
+  end
+end
