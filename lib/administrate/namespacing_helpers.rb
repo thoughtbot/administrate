78e1aad5d88a568ed57cc940c4cbbb8aa0d4f1a2
Rachael Ewins <rachaelewins@hotmail.com>
2021-01-31 19:27:53 +0100

    Fixing Hound issues - variable names shortened

--- lib/administrate/namespacing_helpers.rb
+++ lib/administrate/namespacing_helpers.rb
@@ -5,38 +5,37 @@ module Administrate
     def dashboard_resources_hash
       output = [{}, []]
       dashboard_resources.each do |resource_path|
-        split_path = resource_path.split("/")
+        path = resource_path.split("/")
         output = [
-          add_namespace_to_hash(split_path, output[0]),
-          add_resources_to_hash(split_path, output[1]),
+          add_namespace_to_hash(path, output[0]),
+          add_resources_to_hash(path, output[1]),
         ]
       end
       output
     end
 
-    def add_namespace_to_hash(split_path, current_state)
-      if split_path.length > 1
-        current_state[split_path[0]] ||= [{}, []]
-        current_namespace = current_state[split_path[0]]
-        current_namespace = [
-          add_namespace_to_hash(split_path[1..], current_namespace[0]),
-          add_resources_to_hash(split_path[1..], current_namespace[1]),
+    def add_namespace_to_hash(path, current_state)
+      if path.length > 1
+        current_state[path[0]] ||= [{}, []]
+        current_state[path[0]] = [
+          add_namespace_to_hash(path[1..], current_state[path[0]][0]),
+          add_resources_to_hash(path[1..], current_state[path[0]][1]),
         ]
       end
       current_state
     end
 
-    def add_resources_to_hash(split_path, current_state)
-      if split_path.length == 1
-        current_state.push(split_path[0])
+    def add_resources_to_hash(path, current_state)
+      if path.length == 1
+        current_state.push(path[0])
       end
       current_state
     end
 
-    def generate_resource_routes()
-      namespace = dashboard_resources_hash()
+    def generate_resource_routes
+      namespace = dashboard_resources_hash
       output_string = ""
-      dashboard_resources_hash[1].each do |resource|
+      namespace[1].each do |resource|
         output_string += "#{DEFAULT_INDENT}resources :#{resource}\n"
       end
       output_string += "\n#{generate_namespace_routes(namespace[0])}"
@@ -47,15 +46,15 @@ module Administrate
       output_string = ""
       indentation = DEFAULT_INDENT * indent
       hash.each do |namespace, namespace_resource|
-        nested_namespaces = namespace_resource[0]
-        nested_resources = namespace_resource[1]
+        namespaces = namespace_resource[0]
+        resources = namespace_resource[1]
         output_string += "#{indentation}namespace :#{namespace} do\n"
-        nested_resources.each do |resource|
+        resources.each do |resource|
           resource_string = "#{DEFAULT_INDENT}resources :#{resource}"
           output_string += "#{indentation}#{resource_string}\n"
         end
-        if !nested_namespaces.empty?
-          namespaces_string = generate_namespace_routes(nested_namespaces, indent + 1)
+        if !namespaces.empty?
+          namespaces_string = generate_namespace_routes(namespaces, indent + 1)
           output_string += "\n#{namespaces_string}\n"
         end
         output_string += "#{indentation}end\n"
