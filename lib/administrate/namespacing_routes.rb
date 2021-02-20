ea1fbf170af35e874c022f2f81b0e127dac99d90
Rachael Ewins <rachaelewins@hotmail.com>
2021-02-20 16:33:36 +0100

    Labelled functions private in namespacing_routes

--- lib/administrate/namespacing_routes.rb
+++ lib/administrate/namespacing_routes.rb
@@ -1,6 +1,38 @@
 module Administrate
-  module NamespacingHelpers
+  module NamespacingRoutes
     DEFAULT_INDENT = " " * 2
+
+    def generate_resource_routes
+      namespace = dashboard_resources_hash
+      output_string = ""
+      namespace[1].each do |resource|
+        output_string += "#{DEFAULT_INDENT}resources :#{resource}\n"
+      end
+      output_string += "\n#{generate_namespace_routes(namespace[0])}"
+      output_string
+    end
+
+    private
+
+    def generate_namespace_routes(hash, indent = 1)
+      output_string = ""
+      indentation = DEFAULT_INDENT * indent
+      hash.each do |namespace, namespace_resource|
+        namespaces = namespace_resource[0]
+        resources = namespace_resource[1]
+        output_string += "#{indentation}namespace :#{namespace} do\n"
+        resources.each do |resource|
+          resource_string = "#{DEFAULT_INDENT}resources :#{resource}"
+          output_string += "#{indentation}#{resource_string}\n"
+        end
+        if !namespaces.empty?
+          namespaces_string = generate_namespace_routes(namespaces, indent + 1)
+          output_string += "\n#{namespaces_string}\n"
+        end
+        output_string += "#{indentation}end\n"
+      end
+      output_string
+    end
 
     def dashboard_resources_hash
       output = [{}, []]
@@ -30,36 +62,6 @@ module Administrate
         current_state.push(path[0])
       end
       current_state
-    end
-
-    def generate_resource_routes
-      namespace = dashboard_resources_hash
-      output_string = ""
-      namespace[1].each do |resource|
-        output_string += "#{DEFAULT_INDENT}resources :#{resource}\n"
-      end
-      output_string += "\n#{generate_namespace_routes(namespace[0])}"
-      output_string
-    end
-
-    def generate_namespace_routes(hash, indent = 1)
-      output_string = ""
-      indentation = DEFAULT_INDENT * indent
-      hash.each do |namespace, namespace_resource|
-        namespaces = namespace_resource[0]
-        resources = namespace_resource[1]
-        output_string += "#{indentation}namespace :#{namespace} do\n"
-        resources.each do |resource|
-          resource_string = "#{DEFAULT_INDENT}resources :#{resource}"
-          output_string += "#{indentation}#{resource_string}\n"
-        end
-        if !namespaces.empty?
-          namespaces_string = generate_namespace_routes(namespaces, indent + 1)
-          output_string += "\n#{namespaces_string}\n"
-        end
-        output_string += "#{indentation}end\n"
-      end
-      output_string
     end
   end
 end
