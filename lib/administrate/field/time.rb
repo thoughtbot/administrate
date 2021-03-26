55843ccceab49f7145afef60ad367257c466fcb2
Amy Wall <amywall@thoughtbot.com>
2021-03-26 19:27:16 +0000

    Add format option to time field
    Previously there was no way to specify format for a time field, but you
    can specify a format for date and datetime fields. This commit adds the
    ability to pass a format option to time fields.
    
    Because I18n uses the time locale key for for all objects with a time
    [0], retain the hard-coded time field format where no format option is
    passed to avoid a breaking change.
    
    Addresses issue #1227.
    
    [0] https://github.com/ruby-i18n/i18n/blob/0888807ab2fe4f4c8a4b780f5654a8175df61feb/lib/i18n/backend/base.rb#L82
    
    Co-authored-by: Thom Carter <thom.carter@thoughtbot.com>

--- lib/administrate/field/time.rb
+++ lib/administrate/field/time.rb
@@ -3,6 +3,17 @@ require_relative "base"
 module Administrate
   module Field
     class Time < Base
+      def time
+        return I18n.localize(data, format: format) if options[:format]
+
+        data.strftime("%I:%M%p")
+      end
+
+      private
+
+      def format
+        options[:format]
+      end
     end
   end
 end
