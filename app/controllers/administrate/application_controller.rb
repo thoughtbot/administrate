4a8868dafebc5175b33056faa1f93393977a99c8
Admin <remberth727@gmail.com>
2021-08-23 20:14:30 -0400

    Fix: update resource to symbol for create

--- app/controllers/administrate/application_controller.rb
+++ app/controllers/administrate/application_controller.rb
@@ -86,7 +86,7 @@ module Administrate
     end
 
     def after_resource_created_path(requested_resource)
-      [namespace, requested_resource]
+      [namespace.to_sym, requested_resource]
     end
 
     def after_resource_updated_path(requested_resource)
