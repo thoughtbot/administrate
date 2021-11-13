681a63249e298c96b7a1f441ea36f236d085212b
Pablo Brasero <pablo@pablobm.com>
2021-11-13 07:21:03 +0000

    Avoid Selenium warning about old options format
    No idea of what I'm doing. Documentation is lacking. This seems
    to work. It's taken from https://chromedriver.chromium.org/capabilities

--- spec/support/webdrivers.rb
+++ spec/support/webdrivers.rb
@@ -5,14 +5,19 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.headless!
-  options.add_argument "--window-size=1680,1050"
-
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    options: options,
+    capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      "goog:chromeOptions" => {
+        "args" => [
+          "window-size=1680,1050",
+          "headless",
+          "disable-gpu",
+          "disable-dev-shm-usage",
+        ],
+      },
+    ),
   )
 end
 
