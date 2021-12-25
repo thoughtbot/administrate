17a0961277cb10a707f029580dc7e135679ed40c
Pablo Brasero <pablo@pablobm.com>
2021-12-25 09:05:14 +0000

    Demonstrate (and ensure) custom search

--- spec/example_app/app/controllers/admin/log_entries_controller.rb
+++ spec/example_app/app/controllers/admin/log_entries_controller.rb
@@ -1,4 +1,30 @@
 module Admin
   class LogEntriesController < Admin::ApplicationController
+    def filter_resources(resources, search_term:)
+      return resources if search_term.blank?
+
+      customer_ids = Customer.where(
+        [
+          "name ILIKE ?",
+          "%#{search_term}%",
+        ],
+      ).pluck(:id)
+      order_ids = Order.joins(:customer).where(
+        [
+          "customers.name ILIKE ?",
+          "%#{search_term}%",
+        ],
+      ).pluck(:id)
+
+      customers_filter = resources.where(
+        logeable_type: "Customer",
+        logeable_id: customer_ids,
+      )
+      orders_filter = resources.where(
+        logeable_type: "Order",
+        logeable_id: order_ids,
+      )
+      customers_filter.or(orders_filter)
+    end
   end
 end
