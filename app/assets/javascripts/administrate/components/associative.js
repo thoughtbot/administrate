c9d156bf166dc258deb313128f875c9d789ee73b
Jordan Brough <jordan-brough@users.noreply.github.com>
2022-01-18 14:51:00 -0700

    Create "rinsed-v1" branch (#7)
    * Remove jquery ujs (#1)
    
    * Update HasMany show collection partial path (#2)
    
    Built on https://github.com/thoughtbot/administrate/pull/522
    
    * Enable selectize for select and audience fields (#3 and #5)
    
    * Enable selectize for voucher types field (#6)
    
    * Use and test Ruby 3.0
    
    Co-authored-by: Nic Hippenmeyer <nic@rinsed.co>

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -1,5 +1,8 @@
 $(function() {
+  $('.field-unit--select select').selectize({});
   $('.field-unit--belongs-to select').selectize({});
   $(".field-unit--has-many select").selectize({});
   $('.field-unit--polymorphic select').selectize({});
+  $('.field-unit--audiences-field select').selectize({});
+  $('.field-unit--voucher-types-field select').selectize({});
 });
