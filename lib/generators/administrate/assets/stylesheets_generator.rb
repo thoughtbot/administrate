c82e1037ebc08c74057fd27bfe49dbe23fe16fa4
Matthew Nguyen <contact@n-studio.fr>
2022-02-11 15:59:27 +0800

    lint

--- lib/generators/administrate/assets/stylesheets_generator.rb
+++ lib/generators/administrate/assets/stylesheets_generator.rb
@@ -4,12 +4,13 @@ module Administrate
   module Generators
     module Assets
       class StylesheetsGenerator < Rails::Generators::Base
-        STYLESHEETS_PATH = "lib/assets/stylesheets"
+        STYLESHEETS_PATH = "lib/assets/stylesheets".freeze
 
         source_root File.expand_path("../../../../../", __FILE__)
 
         def copy_stylesheets
-          directory STYLESHEETS_PATH, File.join(STYLESHEETS_PATH, "administrate")
+          destination_path = File.join(STYLESHEETS_PATH, "administrate")
+          directory STYLESHEETS_PATH, destination_path
         end
       end
     end
