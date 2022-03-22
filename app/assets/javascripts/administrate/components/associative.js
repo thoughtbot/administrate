cbe9878ade530c25ac05778c97027fa2adc1a6b0
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2022-03-22 16:35:01 -0400

    Run selectize on wash connect program select field (#25)

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -1,8 +1,9 @@
 $(function() {
-  $('.field-unit--select select').selectize({});
+  $('.field-unit--audiences-field select').selectize({});
   $('.field-unit--belongs-to select').selectize({});
   $(".field-unit--has-many select").selectize({});
   $('.field-unit--polymorphic select').selectize({});
-  $('.field-unit--audiences-field select').selectize({});
+  $('.field-unit--select select').selectize({});
   $('.field-unit--voucher-types-field select').selectize({});
+  $('.field-unit--wash-connect-program-select-field select').selectize({});
 });
