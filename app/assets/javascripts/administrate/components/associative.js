1869ad26992e3421bbc19978f0cee42252cd4ae1
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2022-04-13 13:26:15 -0400

    Run selectize on all select fields

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -1,9 +1,3 @@
 $(function() {
-  $('.field-unit--audiences-field select').selectize({});
-  $('.field-unit--belongs-to select').selectize({});
-  $(".field-unit--has-many select").selectize({});
-  $('.field-unit--polymorphic select').selectize({});
-  $('.field-unit--select select').selectize({});
-  $('.field-unit--voucher-types-field select').selectize({});
-  $('.field-unit--wash-connect-program-select-field select').selectize({});
+  $('.field-unit select').selectize({});
 });
