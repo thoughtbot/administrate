9b1bfcc342cbdbf4c3421bba705ea680d65ce100
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2022-04-13 17:24:39 -0400

    Call .selectize() on all select fields

--- app/assets/javascripts/administrate/components/associative.js
+++ app/assets/javascripts/administrate/components/associative.js
@@ -1,5 +1,3 @@
 $(function() {
-  $('.field-unit--belongs-to select').selectize({});
-  $(".field-unit--has-many select").selectize({});
-  $('.field-unit--polymorphic select').selectize({});
+  $('.field-unit select').selectize({});
 });
--- spec/features/edit_page_spec.rb
+++ spec/features/edit_page_spec.rb
@@ -40,7 +40,8 @@ describe "customer edit page" do
     customer = create(:customer, kind: :standard)
 
     visit edit_admin_customer_path(customer)
-    select "vip", from: "Kind"
+    find("#customer_kind-selectized").click
+    find(".option", text: "vip").click
     click_on "Update Customer"
 
     expect(page).to have_text("KIND")
