0b37fb10cc532fd406d5776a8316451371036c75
Pablo Brasero <pablo@pablobm.com>
2022-04-28 14:16:36 +0100

    Less complex option

--- lib/administrate/custom_dashboard.rb
+++ lib/administrate/custom_dashboard.rb
@@ -4,11 +4,11 @@ module Administrate
 
     class << self
       def resource_name(_opts)
-        named_resource.pluralize.titleize
+        @named_resource.pluralize.titleize
       end
 
       def resource(resource_name)
-        define_singleton_method(:named_resource) { resource_name }
+        @named_resource = resource_name
       end
     end
   end
