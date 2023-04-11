f8793b5a9eef3e891db53e645d2e679286faba0f
Pablo Brasero <pablo@pablobm.com>
2023-04-11 15:57:05 +0100

    Reproduce current behaviour

--- spec/administrate/views/fields/select/_edit_spec.rb
+++ spec/administrate/views/fields/select/_edit_spec.rb
@@ -23,6 +23,168 @@ describe "fields/select/_form", type: :view do
     )
   end
 
+  it "works when :collection is an array" do
+    customer = create(:customer)
+    field = Administrate::Field::Select.new(
+      :kind,
+      "yes",
+      :_page_,
+      resource: customer,
+      collection: ["no", "yes", "absolutely"],
+    )
+
+    render(
+      partial: "fields/select/form",
+      locals: { field: field, f: form_builder(customer) },
+    )
+    expect(rendered).to have_css(
+      %{select[name="customer[kind]"]
+        option[value="yes"][selected="selected"]},
+      text: "yes",
+    )
+  end
+
+  it "works when :collection is a hash" do
+    customer = create(:customer)
+    field = Administrate::Field::Select.new(
+      :kind,
+      "opt1",
+      :_page_,
+      resource: customer,
+      collection: {
+        "no" => "opt0",
+        "yes" => "opt1",
+        "absolutely" => "opt2",
+      },
+    )
+
+    render(
+      partial: "fields/select/form",
+      locals: { field: field, f: form_builder(customer) },
+    )
+    expect(rendered).to have_css(
+      %{select[name="customer[kind]"]
+        option[value="opt1"][selected="selected"]},
+      text: "yes",
+    )
+  end
+
+  it "works when :collection is a call-able" do
+    customer = create(:customer)
+    field = Administrate::Field::Select.new(
+      :kind,
+      "opt1",
+      :_page_,
+      resource: customer,
+      collection: -> {
+        {
+          "no" => "opt0",
+          "yes" => "opt1",
+          "absolutely" => "opt2",
+        }
+      },
+    )
+
+    render(
+      partial: "fields/select/form",
+      locals: { field: field, f: form_builder(customer) },
+    )
+    expect(rendered).to have_css(
+      %{select[name="customer[kind]"]
+        option[value="opt1"][selected="selected"]},
+      text: "yes",
+    )
+  end
+
+  it "provides some context to that call-able" do
+    customer = create(:customer, name: "Dave")
+    field = Administrate::Field::Select.new(
+      :kind,
+      "opt1",
+      :_page_,
+      resource: customer,
+      collection: ->(f) {
+        person = f.resource
+        {
+          "no, #{person.name}" => "opt0",
+          "yes, #{person.name}" => "opt1",
+          "absolutely, #{person.name}" => "opt2",
+        }
+      },
+    )
+
+    render(
+      partial: "fields/select/form",
+      locals: { field: field, f: form_builder(customer) },
+    )
+    expect(rendered).to have_css(
+      %{select[name="customer[kind]"]
+        option[value="opt1"][selected="selected"]},
+      text: "yes, Dave",
+    )
+  end
+
+  it "is an empty list when not set" do
+    customer = create(:customer, name: "Dave")
+    field = Administrate::Field::Select.new(
+      :kind,
+      "opt1",
+      :_page_,
+      resource: customer,
+    )
+
+    render(
+      partial: "fields/select/form",
+      locals: { field: field, f: form_builder(customer) },
+    )
+    expect(rendered).not_to have_css(
+      %{select[name="customer[kind]"] option}
+    )
+  end
+
+  describe "when :include_blank is provided" do
+    it "adds a blank option" do
+      customer = create(:customer, name: "Dave")
+      field = Administrate::Field::Select.new(
+        :kind,
+        "opt1",
+        :_page_,
+        resource: customer,
+        include_blank: "No answer",
+      )
+
+      render(
+        partial: "fields/select/form",
+        locals: { field: field, f: form_builder(customer) },
+      )
+      expect(rendered).to have_css(%{select[name="customer[kind]"] option}, text: "No answer")
+    end
+
+    it "works with a call-able collection" do
+      customer = create(:customer, name: "Dave")
+      field = Administrate::Field::Select.new(
+        :kind,
+        "opt1",
+        :_page_,
+        resource: customer,
+        collection: -> {
+          {
+            "no" => "opt0",
+            "yes" => "opt1",
+            "absolutely" => "opt2",
+          }
+        },
+        include_blank: "No answer",
+      )
+
+      render(
+        partial: "fields/select/form",
+        locals: { field: field, f: form_builder(customer) },
+      )
+      expect(rendered).to have_css(%{select[name="customer[kind]"] option}, text: "No answer")
+    end
+  end
+
   def form_builder(object)
     ActionView::Helpers::FormBuilder.new(
       object.model_name.singular,
