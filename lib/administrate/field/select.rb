97fbc0eb1ca4a4bad0f803a4d31e108a0089d9a5
Pablo Brasero <pablo@pablobm.com>
2023-04-13 09:40:07 +0100

    Avoid logic in the template

--- lib/administrate/field/select.rb
+++ lib/administrate/field/select.rb
@@ -8,22 +8,17 @@ module Administrate
       end
 
       def selectable_options
-        collection
+        values = options.fetch(:collection, [])
+
+        if values.respond_to? :call
+          values = values.arity.positive? ? values.call(self) : values.call
+        end
+
+        values
       end
 
       def include_blank_option
         options.fetch(:include_blank, false)
-      end
-
-      private
-
-      def collection
-        values = options.fetch(:collection, [])
-        if values.respond_to? :call
-          return values.arity.positive? ? values.call(self) : values.call
-        end
-
-        @collection ||= values
       end
     end
   end
