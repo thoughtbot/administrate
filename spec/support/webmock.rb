cefd465df375d550a0c77bef8943688274f44578
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-08-04 15:11:00 -0700

    Upgrade selenium-webdriver gem and remove webdrivers

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,11 +1,7 @@
 require "webmock/rspec"
 
-# Allow downloading webdrivers for Selenium
-driver_hosts = Webdrivers::Common.subclasses.
-  map { |driver| URI(driver.base_url).host }
-
 # Downloading the Firefox driver involves a redirect
-driver_hosts += ["github-releases.githubusercontent.com"]
+driver_hosts = ["github-releases.githubusercontent.com"]
 
 # Additionally, avoid conflict with Selenium (localhost)
 WebMock.disable_net_connect!(allow_localhost: true, allow: driver_hosts)
