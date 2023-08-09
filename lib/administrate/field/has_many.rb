a09a3361407f51cd326417d305f86e434e1aa097
jubilee2 <jubilee2@gmail.com>
2023-08-09 20:18:01 +0000

    fix

--- lib/administrate/field/has_many.rb
+++ lib/administrate/field/has_many.rb
@@ -23,8 +23,9 @@ module Administrate
 
       def associated_collection(order = self.order)
         Administrate::Page::Collection.new(
-          associated_dashboard, order:,
-          collection_attributes: options[:collection_attributes])
+          associated_dashboard, order: order,
+          collection_attributes: options[:collection_attributes]
+        )
       end
 
       def attribute_key
