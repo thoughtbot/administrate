641f2c1f33e5eac4dc6996eb113c6f565ad8a0fa
nakamura <shop@goosys.sakura.ne.jp>
2023-11-10 13:26:59 +0000

    Add context to Page

--- spec/dashboards/order_dashboard_spec.rb
+++ spec/dashboards/order_dashboard_spec.rb
@@ -8,10 +8,70 @@ describe OrderDashboard do
       expect(dashboard.permitted_attributes).to include(:address_line_one)
     end
 
-    it "returns the attribute_id name for belongs_to relationships" do
-      dashboard = OrderDashboard.new
+    context "when the page context is 'new' or 'create'" do
+      let(:ctx_with_non_admin_user) do
+        Struct.new(:pundit_user).new(Struct.new(:admin?).new(false))
+      end
+      let(:ctx_with_admin_user) do
+        Struct.new(:pundit_user).new(Struct.new(:admin?).new(true))
+      end
 
-      expect(dashboard.permitted_attributes).to include("customer_id")
+      context "when the user is not an admin" do
+        it "not returns attributes with customer_id" do
+          dashboard = OrderDashboard.new
+          expect(
+            dashboard.permitted_attributes("new", ctx_with_non_admin_user)
+          ).not_to include("customer_id")
+          expect(
+            dashboard.permitted_attributes("create", ctx_with_non_admin_user)
+          ).not_to include("customer_id")
+        end
+      end
+
+      context "when the user is an admin" do
+        it "returns attributes with customer_id" do
+          dashboard = OrderDashboard.new
+          expect(
+            dashboard.permitted_attributes("new", ctx_with_admin_user)
+          ).to include("customer_id")
+          expect(
+            dashboard.permitted_attributes("create", ctx_with_admin_user)
+          ).to include("customer_id")
+        end
+      end
+    end
+
+    context "when the page context is 'edit' or 'update'" do
+      let(:ctx_with_non_admin_user) do
+        Struct.new(:pundit_user).new(Struct.new(:admin?).new(false))
+      end
+      let(:ctx_with_admin_user) do
+        Struct.new(:pundit_user).new(Struct.new(:admin?).new(true))
+      end
+
+      context "when the user is not an admin" do
+        it "not returns attributes with customer_id" do
+          dashboard = OrderDashboard.new
+          expect(
+            dashboard.permitted_attributes("edit", ctx_with_non_admin_user)
+          ).not_to include("customer_id")
+          expect(
+            dashboard.permitted_attributes("update", ctx_with_non_admin_user)
+          ).not_to include("customer_id")
+        end
+      end
+
+      context "when the user is an admin" do
+        it "also no returns attributes with customer_id" do
+          dashboard = OrderDashboard.new
+          expect(
+            dashboard.permitted_attributes("edit", ctx_with_admin_user)
+          ).not_to include("customer_id")
+          expect(
+            dashboard.permitted_attributes("update", ctx_with_admin_user)
+          ).not_to include("customer_id")
+        end
+      end
     end
   end
 end
