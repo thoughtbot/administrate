f54ada55e8eee60191a9e33a2830e532887348dd
nakamura <shop@goosys.sakura.ne.jp>
2023-12-26 06:39:22 +0000

    Add resource instance variables and audit log sample

--- app/controllers/administrate/application_controller.rb
+++ app/controllers/administrate/application_controller.rb
@@ -9,6 +9,7 @@ module Administrate
       resources = apply_collection_includes(resources)
       resources = order.apply(resources)
       resources = paginate_resources(resources)
+      @resources = resources
       page = Administrate::Page::Collection.new(dashboard, order: order)
 
       render locals: {
@@ -26,7 +27,7 @@ module Administrate
     end
 
     def new
-      resource = new_resource
+      @new_resource = resource = new_resource
       authorize_resource(resource)
       render locals: {
         page: Administrate::Page::Form.new(dashboard, resource)
@@ -40,7 +41,7 @@ module Administrate
     end
 
     def create
-      resource = new_resource(resource_params)
+      @new_resource = resource = new_resource(resource_params)
       authorize_resource(resource)
 
       if resource.save
