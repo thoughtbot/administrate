e39bd90d86dcc3b376c87d2b8c89e79185cdeb96
Kiril Mitov <kiril@retreaver.com>
2024-05-25 11:06:25 +0300

    Documentation says Field::URL accepts option for truncate
    https://administrate-demo.herokuapp.com/customizing_dashboards but code is different.
    
    Showing truncated data for urls.

--- spec/administrate/views/fields/url/_index_spec.rb
+++ spec/administrate/views/fields/url/_index_spec.rb
@@ -2,13 +2,14 @@ require "rails_helper"
 
 describe "fields/url/_index", type: :view do
   let(:product) do
-    build(:product, image_url: "https://thoughtbot.com/image.jpg")
+    build(:product, image_url: "https://thoughtbot.com/image-but-extremely-long-and-should-be-truncated.jpg")
   end
 
   it "renders url" do
     url = instance_double(
       "Administrate::Field::Url",
       data: product.image_url,
+      truncate: product.image_url[0..50],
       html_options: {}
     )
 
@@ -19,7 +20,7 @@ describe "fields/url/_index", type: :view do
 
     expect(rendered).to have_css(
       %(a[href="#{product.image_url}"]),
-      text: product.image_url
+      text: product.image_url[0..50]
     )
   end
 
@@ -27,6 +28,7 @@ describe "fields/url/_index", type: :view do
     url = instance_double(
       "Administrate::Field::Url",
       data: product.image_url,
+      truncate: product.image_url,
       html_options: {referrerpolicy: "no-referrer"}
     )
 
