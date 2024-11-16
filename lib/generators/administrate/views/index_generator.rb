c1f78d0ec9bb87420ee7275d531122bbe9ec6068
nakamura <shop@goosys.sakura.ne.jp>
2024-11-16 23:32:18 +0900

    Updated Views::IndexGenerator to copy all partials (#2452)

--- lib/generators/administrate/views/index_generator.rb
+++ lib/generators/administrate/views/index_generator.rb
@@ -9,7 +9,11 @@ module Administrate
         def copy_template
           copy_resource_template("index")
           copy_resource_template("_collection")
+          copy_resource_template("_collection_header_actions")
+          copy_resource_template("_collection_item_actions")
           copy_resource_template("_index_header")
+          copy_resource_template("_pagination")
+          copy_resource_template("_search")
         end
       end
     end
