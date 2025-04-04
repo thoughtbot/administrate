4cb0bca59e202184feedb35b5bbddc9908c3544e
Nick Charlton <nick@nickcharlton.net>
2025-04-04 16:50:55 +0100

    Get a real test working

--- spec/black_box/default_rails_app_spec.rb
+++ spec/black_box/default_rails_app_spec.rb
@@ -4,65 +4,31 @@ RSpec.describe "with a Default Rails app" do
   it "works" do
     setup_capybara
     create_rails_application
+    setup_post_model
+    setup_administrate
 
-    # setup_post_model
-    # add_administrate
-    # generate_administrate_dashboards
+    app.start_and_wait_for_ready
 
-    # future: we don't handle the case where rails s fails to start because
-    # there's something already running
+    visit("/admin")
 
-    start_and_wait_for_ready
+    expect(page).to have_css("header h1#page-title", text: "Posts")
+    click_on "New post"
 
-    visit("/")
+    fill_in "Body", with: "Some words!"
+    fill_in "Title", with: "A post with words"
+    click_button "Create Post"
 
-    expect(page).to have_content("Rails version: 8.0.2")
+    expect(page).to have_content("Post was successfully created.")
+    expect(page).to have_content("Show Post #1")
 
-    process.stop
-  end
-
-  def create_rails_application
-    session.create_file("Gemfile", <<~RUBY)
-      source "http://rubygems.org"
-
-      gem "rails"
-    RUBY
-
-    session.run("bundle install")
-
-    rails_new_cmd = [
-      "bundle exec rails new .",
-      "--skip-test",
-      "--skip-spring",
-      "--force"
-    ].join(" ")
-
-    expect(session.run(rails_new_cmd))
-      .to be_a_success.and have_stdout("force  Gemfile")
+    app.stop
   end
 
   def session
     @session ||= JetBlack::Session.new(options: {clean_bundler_env: true})
   end
 
-  def start_and_wait_for_ready
-    Bundler.with_unbundled_env do
-      process.add_handler(:ready, "Use Ctrl-C to stop\n")
-      process.start
-      process.wait_for_handler(:ready)
-    end
-  end
-
-  def process
-    @process ||=
-      Bundler.with_unbundled_env do
-        SackRace::Process.new(
-          "bundle exec rails server",
-          {
-            chdir: session.directory,
-            verbose: true
-          }
-        )
-      end
+  def app
+    @app ||= RailsProcess.new(session)
   end
 end
