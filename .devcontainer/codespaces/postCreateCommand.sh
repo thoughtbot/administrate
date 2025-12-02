3b32963bfdf1f47f400dd45301d3bf981b9ba84a
nakamura <shop@goosys.sakura.ne.jp>
2025-12-02 17:14:46 +0000

    Update configuration for prebuild and rebuild compatibility

--- .devcontainer/codespaces/postCreateCommand.sh
+++ .devcontainer/codespaces/postCreateCommand.sh
@@ -1,4 +1,6 @@
 
+until docker info >/dev/null 2>&1; do sleep 1; done
+docker rm postgres >/dev/null 2>&1
 docker run -d -i --name postgres -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 until docker exec postgres pg_isready -U postgres; do sleep 1; done
 
