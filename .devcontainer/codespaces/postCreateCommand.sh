926d264e086b57748a7749dbe57ffa1542921a0f
nakamura <shop@goosys.sakura.ne.jp>
2025-12-24 04:56:30 +0000

    Remove addition of ENV variables to .env

--- .devcontainer/codespaces/postCreateCommand.sh
+++ .devcontainer/codespaces/postCreateCommand.sh
@@ -4,10 +4,11 @@ docker rm postgres >/dev/null 2>&1
 docker run -d -i --name postgres -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 until docker exec postgres pg_isready -U postgres; do sleep 1; done
 
+# Create config/database.yml
 ./bin/setup
+# Setup &  Create .env
 ./bin/setup
-RAILS_ENV=test bundle exec rake db:setup dev:prime
 
-echo "" >> .env && echo "DATABASE_URL=postgresql://postgres:@localhost" >> .env
+RAILS_ENV=test bundle exec rake db:setup dev:prime
 
 docker stop postgres
