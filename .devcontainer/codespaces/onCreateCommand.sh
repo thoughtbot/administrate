8585d9ee0b3e9599655eff24ef68c403361be6b4
nakamura <shop@goosys.sakura.ne.jp>
2026-01-07 04:18:31 +0000

    add labels

--- .devcontainer/codespaces/onCreateCommand.sh
+++ .devcontainer/codespaces/onCreateCommand.sh
@@ -12,17 +12,22 @@ rvm --default use 4.0.0
 rvm list
 ruby --version
 
+# Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome.deb
 sudo apt update && sudo apt install -y /tmp/google-chrome.deb && rm /tmp/google-chrome.deb
 google-chrome --version
 
+# Install Chromedriver
 CHROMEDRIVER_VERSION=$(google-chrome --version | awk '{print $3}')
 wget https://storage.googleapis.com/chrome-for-testing-public/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 sudo mv chromedriver-linux64/chromedriver /usr/local/bin/
 rm -rf chromedriver-linux64.zip chromedriver-linux64
+chromedriver --version
 
+# Install libs
 sudo apt install -y libvips
 
+# Cleanup
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
