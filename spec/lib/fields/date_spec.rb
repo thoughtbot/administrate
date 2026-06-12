f7b44dbf8c3741ce268ec8458c21df679e125165
nakamura <shop@goosys.sakura.ne.jp>
2026-06-12 14:54:54 +0900

    Add administrate default date/time formats (#2781)
    - refs #2702.
    - The only way to change the time format was to modify the Rails default
      setting. However, changing it would also affect other parts of the
      application outside the admin panel. Therefore, I added a dedicated format
      specifically for Administrate.
      - `administrate_date_default` `administrate_datetime_default`
        `administrate_time_default`.
    - Currently, the Time format is fixed as `"%I:%M%p"`, but in Japanese, we
      would prefer the order `"%p %I:%M"`. I believe other languages may also
      have their own preferred orders. I have made it possible to change this for
      each language using `administrate_time_default`.

--- spec/lib/fields/date_spec.rb
+++ spec/lib/fields/date_spec.rb
@@ -6,12 +6,39 @@ describe Administrate::Field::Date do
   let(:formats) do
     {
       date: {
-        formats: {default: "%m/%d/%Y", short: "%b %d"},
+        formats: {
+          default: "%m/%d/%Y",
+          short: "%b %d",
+          administrate_date_default: "%m/%d, %Y"
+        },
         abbr_month_names: Array.new(13) { |i| "Dec" if i == 12 },
         abbr_day_names: Array.new(7) { |i| "Fri" if i == 5 }
       },
       time: {
-        formats: {default: "%a, %b %-d, %Y", short: "%d %b"}
+        formats: {
+          default: "%a, %b %-d, %Y at %r",
+          short: "%d %b %H:%M",
+          administrate_datetime_default: "%a, %b %-d, %Y, %r",
+          administrate_time_default: "%I:%M%p"
+        }
+      }
+    }
+  end
+  let(:formats_without_administrate_default) do
+    {
+      date: {
+        formats: {
+          default: "%m/%d/%Y",
+          short: "%b %d"
+        },
+        abbr_month_names: Array.new(13) { |i| "Dec" if i == 12 },
+        abbr_day_names: Array.new(7) { |i| "Fri" if i == 5 }
+      },
+      time: {
+        formats: {
+          default: "%a, %b %-d, %Y at %r",
+          short: "%d %b %H:%M"
+        }
       }
     }
   end
@@ -21,11 +48,11 @@ describe Administrate::Field::Date do
       with_translations(:en, formats) do
         field = Administrate::Field::Date
           .new(:start_date, start_date, :show)
-        expect(field.date).to eq("12/25/2015")
+        expect(field.date).to eq("12/25, 2015")
       end
     end
 
-    context "with `prefix` option" do
+    context "with `format` option" do
       it "displays the date in the requested format" do
         options_field = Administrate::Field::Date
           .with_options(format: :short)
@@ -43,6 +70,35 @@ describe Administrate::Field::Date do
 
         with_translations(:en, formats) do
           expect(field.date).to eq("2015")
+        end
+      end
+    end
+
+    context "without `format` option" do
+      it "falls back to default format if administrate_date_default is missing" do
+        options_field = Administrate::Field::Date
+        field = options_field.new(:start_date, start_date, :show)
+
+        with_translations(:en, formats_without_administrate_default) do
+          expect(field.date).to eq("12/25/2015")
+        end
+      end
+
+      it "falls back to static format (%Y-%m-%d) if locale formats are missing" do
+        options_field = Administrate::Field::Date
+        field = options_field.new(:start_date, start_date, :show)
+
+        with_translations(:unformatted_locale, {}) do
+          available_locales = I18n.available_locales
+          locale = I18n.locale
+          begin
+            I18n.available_locales = [:unformatted_locale]
+            I18n.locale = :unformatted_locale
+            expect(field.date).to eq("2015-12-25")
+          ensure
+            I18n.available_locales = available_locales
+            I18n.locale = locale
+          end
         end
       end
     end
--- spec/lib/fields/date_time_spec.rb
+++ spec/lib/fields/date_time_spec.rb
@@ -6,26 +6,78 @@ describe Administrate::Field::DateTime do
   let(:formats) do
     {
       date: {
-        formats: {default: "%m/%d/%Y", short: "%b %d"},
+        formats: {
+          default: "%m/%d/%Y",
+          short: "%b %d",
+          administrate_date_default: "%m/%d, %Y"
+        },
         abbr_month_names: Array.new(13) { |i| "Dec" if i == 12 },
         abbr_day_names: Array.new(7) { |i| "Fri" if i == 5 }
       },
       time: {
-        formats: {default: "%a, %b %-d, %Y at %r", short: "%d %b %H:%M"}
+        formats: {
+          default: "%a, %b %-d, %Y at %r",
+          short: "%d %b %H:%M",
+          administrate_datetime_default: "%a, %b %-d, %Y, %r",
+          administrate_time_default: "%I:%M%p"
+        }
+      }
+    }
+  end
+  let(:formats_without_administrate_default) do
+    {
+      date: {
+        formats: {
+          default: "%m/%d/%Y",
+          short: "%b %d"
+        },
+        abbr_month_names: Array.new(13) { |i| "Dec" if i == 12 },
+        abbr_day_names: Array.new(7) { |i| "Fri" if i == 5 }
+      },
+      time: {
+        formats: {
+          default: "%a, %b %-d, %Y at %r",
+          short: "%d %b %H:%M"
+        }
       }
     }
   end
 
   describe "#date" do
-    it "displays the date" do
-      with_translations(:en, formats) do
-        field = Administrate::Field::DateTime
-          .new(:start_date, start_date, :show)
-        expect(field.date).to eq("12/25/2015")
+    context "without `format` option" do
+      it "displays the date in the administrate default format" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+        with_translations(:en, formats) do
+          expect(field.date).to eq("12/25, 2015")
+        end
+      end
+
+      it "falls back to default format if administrate_date_default is missing" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+        with_translations(:en, formats_without_administrate_default) do
+          expect(field.date).to eq("12/25/2015")
+        end
+      end
+
+      it "falls back to static format (%Y-%m-%d) if locale formats are missing" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+
+        with_translations(:unformatted_locale, {}) do
+          available_locales = I18n.available_locales
+          locale = I18n.locale
+          begin
+            I18n.available_locales = [:unformatted_locale]
+            I18n.locale = :unformatted_locale
+            expect(field.date).to eq("2015-12-25")
+          ensure
+            I18n.available_locales = available_locales
+            I18n.locale = locale
+          end
+        end
       end
     end
 
-    context "with `prefix` option" do
+    context "with `format` option" do
       it "displays the date in the requested format" do
         options_field = Administrate::Field::DateTime
           .with_options(format: :short)
@@ -90,15 +142,40 @@ describe Administrate::Field::DateTime do
   end
 
   describe "#datetime" do
-    it "displays the datetime" do
-      field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+    context "without `format` option" do
+      it "displays the datetime in the administrate default format" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+        with_translations(:en, formats) do
+          expect(field.datetime).to eq("Fri, Dec 25, 2015, 10:15:45 AM")
+        end
+      end
 
-      with_translations(:en, formats) do
-        expect(field.datetime).to eq("Fri, Dec 25, 2015 at 10:15:45 AM")
+      it "falls back to default format if administrate_datetime_default is missing" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+        with_translations(:en, formats_without_administrate_default) do
+          expect(field.datetime).to eq("Fri, Dec 25, 2015 at 10:15:45 AM")
+        end
+      end
+
+      it "falls back to static format (%Y-%m-%d %H:%M) if locale formats are missing" do
+        field = Administrate::Field::DateTime.new(:start_date, start_date, :show)
+
+        with_translations(:unformatted_locale, {}) do
+          available_locales = I18n.available_locales
+          locale = I18n.locale
+          begin
+            I18n.available_locales = [:unformatted_locale]
+            I18n.locale = :unformatted_locale
+            expect(field.datetime).to eq("2015-12-25 10:15")
+          ensure
+            I18n.available_locales = available_locales
+            I18n.locale = locale
+          end
+        end
       end
     end
 
-    context "with `prefix` option" do
+    context "with `format` option" do
       it "displays the datetime in the requested format" do
         options_field = Administrate::Field::DateTime
           .with_options(format: :short)
